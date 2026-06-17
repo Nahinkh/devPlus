d89f1c9be565cc719fcc36262a47abe7c595f980
NahinKh <nahinkhan77@gmail.com>
2026-06-17 16:41:23 +0600

    Complete Singup and login setup accroding to provided requirement

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -50,6 +50,7 @@ const loginUserIntoDB = async (email: string, password: string) => {
   const jwtPayload = {
     id: user.id,
     name: user.name,
+    email: user.email,
     role: user.role,
     created_at: user.created_at,
     updated_at: user.updated_at,
